4927e86718b86a16479e06b774bb299f1d8a127c
maxoldgood <maxoldgood@gmail.com>
2024-03-09 21:29:23 +0700

    Fix: product card ui

--- src/widgets/product-card/index.tsx
+++ src/widgets/product-card/index.tsx
@@ -13,10 +13,12 @@ export function ProductCard(props: ProductCardProps) {
     <div className={styles.product_card}>
       <img className={styles.product_card__image} src={image} alt={title} width={293} height={293} />
       <div className={styles.product_card__header}>
-        <span className={styles.product_card__name}>{title}</span>
-        <Like className={styles.product_card__like} />
+        <div className={styles.product_card__title}>
+          <p className={styles.product_card__name}>{title}</p>
+          <Like className={styles.product_card__like} />
+        </div>
+        <p className={styles.product_card__price}>${price}</p>
       </div>
-      <span className={styles.product_card__price}>${price}</span>
     </div>
   )
 }
