4c4dfd362c0036df3b5ad554cf17fd07fd6e6bb5
maxoldgood <maxoldgood@gmail.com>
2024-03-29 19:06:26 +0700

    fix: button propagation

--- src/entities/product/ui/product-card.tsx
+++ src/entities/product/ui/product-card.tsx
@@ -13,21 +13,24 @@ export function ProductCard(props: ProductCardProps) {
   const { product, actionSlot, className } = props
   const { title, price, images } = product
   return (
-    <Link to={`product/${product.id}`}>
-      <div className={`${styles.product_card} ${styles[className ? className : '']}`}>
+    <div className={`${styles.product_card} ${styles[className ? className : '']}`}>
+      <Link to={`product/${product.id}`}>
         <div className={styles.product_card__image_wrapper}>
           <img className={styles.product_card__image} src={images[0]} alt={title} loading="lazy" />
         </div>
-        <div className={styles.product_card__header}>
+      </Link>
+
+      <div className={styles.product_card__header}>
+        <Link to={`product/${product.id}`}>
           <div className={styles.product_card__title}>
             <p className={styles.product_card__name}>{title}</p>
           </div>
-          <div className={styles.product_card__bottom}>
-            <p className={styles.product_card__price}>${price}</p>
-            {actionSlot && <div className={styles.product_card__action}>{actionSlot}</div>}
-          </div>
+        </Link>
+        <div className={styles.product_card__bottom}>
+          <p className={styles.product_card__price}>${price}</p>
+          {actionSlot && <div className={styles.product_card__action}>{actionSlot}</div>}
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
