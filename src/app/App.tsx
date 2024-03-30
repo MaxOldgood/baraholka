e8c7d9e51ecb7a345926661c6be882dc9b55fec9
maxoldgood <maxoldgood@gmail.com>
2024-03-30 18:02:24 +0700

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,17 @@ import { Layout } from '../shared/ui/layout/'
 import { Header } from '../widgets/header/header'
 
 function App() {
+  // Функция устанавливает динамическую высоту видимой области
+  function setViewportProperty() {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }
+
+  // Реагируем на изменение размеров экрана
+  window.addEventListener('resize', setViewportProperty)
+
+  // Выполняем инициализацию
+  setViewportProperty()
   return <Layout header={<Header />} />
 }
 
